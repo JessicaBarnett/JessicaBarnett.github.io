12279843daea14212e8ef858fa2d34532b82e79e
JessicaBarnett <BarnettEffect@gmail.com>
2014-09-09 00:14:42 -0400

    overhaul of about page done.  lots of tweaking.  swapped skills and projects sections backgrounds again.

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(window).scroll(hideAbout);
+
+function hideAbout(e){
+	var position = $(window).scrollTop();
+
+	if 	(position > 490 && position < 1600) 
+		$("#about .fixed").css("opacity", 1);
+	else
+		$("#about .fixed").css("opacity", 0);
+}
+
+
+//testing scroll position
+// $(window).scroll(function () { 
+// 	var scroll = $(window).scrollTop();
+// 	$("nav p").remove();
+// 	$("nav").append($("<p></p>").text("pixels scrolled:  " + scroll));
+// });
