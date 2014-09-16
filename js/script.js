b8d9ac7f3c07694518db448547ad7aca16541c74
JessicaBarnett <BarnettEffect@gmail.com>
2014-09-16 02:39:28 -0400

    lots and lots of mobile tweaks.  lots more still to do.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,38 @@
-$(window).scroll(hideAbout);
+//(function(){
+var showAt, hideAt;
+var position;
 
-function hideAbout(e){
-	var position = $(window).scrollTop();
+mqTab = window.matchMedia("(max-width: 768px) and (min-width: 600px)");
+mqPhone = window.matchMedia("(max-width: 600px)");
 
-	if 	(position > 400 && position < 1600) 
+hideAbout();
+$(window).scroll(scrollHandler);
+
+function scrollHandler(e){
+	setHidePoints();
+	position = $(window).scrollTop();
+
+	hideAbout(position);
+}
+
+function hideAbout(position){
+	if(mqPhone.matches)
+		$("#about .fixed").css("opacity", 1);
+	else if (position > showAt && position < hideAt)
 		$("#about .fixed").css("opacity", 1);
 	else
 		$("#about .fixed").css("opacity", 0);
 }
 
+function setHidePoints(){
+	if (mqTab.matches){
+		showAt = 800;
+		hideAt = 3000;
+	}
+	else{
+		showAt = 600;
+		hideAt = 2000;
+	}
+}
 
-//testing scroll position
-// $(window).scroll(function () { 
-// 	var scroll = $(window).scrollTop();
-// 	$("nav p").remove();
-// 	$("nav").append($("<p></p>").text("pixels scrolled:  " + scroll));
-// });
+// })(); 
