94b6b74630ceb0ba6f574c22f139d4b4b2f6039a
Jessica Barnett <barnetteffect@gmail.com>
2024-12-19 14:24:45 -0500

    genIcons gens stories as well

---
+++ src/stories/Icons.stories.ts
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+import { XXXX } from 'XXXX';
+
+const meta = {
+  title: 'Icons/XXXX',
+  component: XXXX,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {},
+  args: {},
+} satisfies Meta<typeof XXX>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
