d0487e609142c0b95a0ee79d54c7f4517bd936b4
Jessica Barnett <barnetteffect@gmail.com>
2025-01-15 16:46:19 -0500

    copy edits

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -11,11 +11,11 @@ function AboutSection() {
                 </p>
                 <div className="paragraph-group">
                     <p className="indent-2 deco-font-3 ">
-                        I've been crafting <em>beautiful user experiences</em> since 2014, and <em>high-quality node apps</em> since 2021.  I strive to <em>use technology to make people's lives easier</em>, one tappity-tap at a time.
+                        I've been crafting <em>beautiful user experiences</em> since 2014 and <em>high-quality node apps</em> since 2021.  I strive to <em>use technology to make people's lives easier</em>, one tappity-tap at a time.
                     </p>
                 </div>
                 <p className="deco-font-4 emphasized-p">
-                    Want to work with me?  Great!  Drop me a line using the <a className="link" href="#contact">contact form</a>!  Otherwise, Feel free to peruse my <a className="link" href="#projects">projects</a> and <a className="link" href="#projects">experience</a> below.
+                    Want to work with me?  Great!  Drop me a line using the <a className="link" href="#contact">contact form</a>;  Otherwise, Feel free to peruse my <a className="link" href="#projects">projects</a> and <a className="link" href="#projects">experience</a> below.
                 </p>
             </div>
 
