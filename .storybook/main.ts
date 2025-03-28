95df72a5764d87bc30c1272a206fb9b85c05adb1
Jessica Barnett <barnetteffect@gmail.com>
2025-03-28 15:08:57 -0400

    add a roadmap page to the storybook.  Change the bg of the dialog so that it matches the main page.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: [
-    "../stories/**/*.mdx",
+    "../stories/**!(manifests)/*.mdx",
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
